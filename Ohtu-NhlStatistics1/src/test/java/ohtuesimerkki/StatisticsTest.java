97dd68116573a468818f3ffd3439c952c6d9bf0b
Lydia Lairala <lydia.lairala@helsinki.fi>
2013-03-24 18:54:23 +0200

    commit

---
+++ Ohtu-NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,49 @@
+package ohtuesimerkki;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lydia Lairala
+ */
+public class StatisticsTest {
+
+    public StatisticsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSearch() {
+        assertTrue(1 == 1);
+    }
+
+    @Test
+    public void team() {
+        assertTrue(1 == 1);
+    }
+
+    @Test
+    public void topScorers() {
+        assertTrue(1 == 1);
+    }
+}
